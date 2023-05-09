9ab2dd8fa44f893ce27fe1aeaded187f6e706e57
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-09 20:21:56 +0100

    feat: added Description and Thumbnails components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 'use client'
+import SectionHeader from '../components/SectionHeader/index';
 import * as S from './styles';
 import { MenuFold } from '@styled-icons/remix-fill';
-
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <S.Navbar>
         <S.List>
-          <S.Header>Jean Francois Portfolio</S.Header>
+          <S.Header>Jean&apos;s Portfolio</S.Header>
           <div>
             <MenuFold size={32} color='#F7EDE7' />
           </div>
@@ -17,11 +18,28 @@ export default function Home() {
       <S.Main>
         <S.Hero>
           <div>
-            <S.HeroHeader>Hello there, I'm Jean! Self-thaught webdeveloper </S.HeroHeader>
+            <S.HeroHeader>Hello there, I&apos;m Jean<span>! </span> Self-thaught web developer<span>. </span> </S.HeroHeader>
           </div>
-          <S.MainImage src={"/ilustration_char_1.webp"} alt='image 1' width={250} height={250}></S.MainImage>
+          <S.MainImage src={"/ilustration_char_1.webp"} alt='image 1' width={300} height={320}></S.MainImage>
         </S.Hero>
+        <S.Description>
+          <SectionHeader text="Who am I <span> ? </span>" />
+          <S.Paragraph>Born in Luxembourg, living in Portugal<span>. </span> This journey started 3 years ago and keeps going<span>. </span>
+            Check the carousel below to know more about me<span>. </span>
+          </S.Paragraph>
+          <S.Images>
+            <S.ThumbImage height={230} width={230} alt='a watercolor painting of a dog with a red bandana near the shore' src={"/dog.webp"} />
+            <S.ThumbImage height={230} width={230} alt='a watercolor painting of a coder working sitting at his desk' src={"/desk.webp"} />
+            <S.ThumbImage height={230} width={230} alt='a watercolor painting of a gamer playing a videogame with enthusiasm' src={"/gamer.webp"} />
+            <S.ThumbImage height={230} width={230} alt='a watercolor painting of a basketball player aiming at the hoop' src={"/basket.webp"} />
+            <S.ThumbImage height={230} width={230} alt='a watercolor painting of a book and a hot coffee' src={"/bookandcoffee.webp"} />
+
+
+          </S.Images>
+        </S.Description>
+
       </S.Main>
+
     </>
   )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import Image from 'next/image';
+import layer from "../../public/layer.svg"
+
 
 export const Navbar = styled.nav`
     padding: 1.4rem;
@@ -13,7 +15,7 @@ export const List = styled.ul`
 `
 
 export const Header = styled.h1`
-    font-size: 2rem;
+    font-size: 2.4rem;
     color:#F7EDE7;
 `
 
@@ -26,13 +28,55 @@ export const Hero = styled.div`
     display: flex;
     justify-content: space-around;
     background-color: #F7EDE7;
+    overflow-x: hidden;
     `
 export const HeroHeader = styled.h2`
-    font-size: 2rem;
+    font-size: 2.4rem;
     padding: 3rem 0 0 3rem;
     color: #123C73;
+
+    & span{
+        color:red;
+    }
 `
 
 export const MainImage = styled(Image)`
     clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 0% 100%);
+    margin-right: -100px;
+`
+
+export const Description = styled.div`
+    background-image: url("/layer.png");
+    padding: 2rem 3rem 3rem 3rem;
+    margin-bottom: 3rem;
+    position: relative;
+    
+    /* & :before{
+        content: url("/11.png");
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        clip-path: circle(50% at 50% 50%);
+    } */
+`
+
+export const Paragraph = styled.p`
+    font-size: 1.8rem;
+    color:#F7EDE7;
+
+    & span{
+        color:#D2042D;
+    }  
+`
+
+export const Images = styled.div`
+    margin-top: 2rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: space-evenly;   
+`
+
+export const ThumbImage = styled(Image)`
+clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);
 `
