37f536144b42ce3f897b33c164c74756140c6191
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-17 20:34:13 +0100

    feat: added Typewriter, styles & config

---
+++ src/components/Typewriter/index.tsx
@@ -0,0 +1,23 @@
+import * as S from "./styles"
+import Typewriter from 'typewriter-effect';
+const TypeWriter = () => {
+    return (<Typewriter
+        onInit={(typewriter) => {
+            typewriter.typeString('Hello there, I&apos;m Jean<span>!</span> Front-end developer<span>.</span> ')
+                .pauseFor(1000)
+                .typeString('36 years old')
+                .pauseFor(1700)
+                .changeDeleteSpeed(0.4)
+                .deleteChars(12)
+                .typeString('With the looks of a 25 years old<span>.</span> ')
+                .pauseFor(1000)
+                .typeString('Under my belt, around 3 years of experience<span>.</span>')
+                .pauseFor(1000)
+                .changeDeleteSpeed(0.1)
+                .deleteChars(121)
+                .start();
+        }}
+    />)
+}
+
+export default TypeWriter
---
+++ src/components/Typewriter/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import Typewriter from 'typewriter-effect';
+
+export const Textwriter = styled(Typewriter)`
+
+`; 
